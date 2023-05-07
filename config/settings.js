e02a4a1e8690a4cf89a4c2e1f578eb3f107197df
Prajith <prajithreddym@gmail.com>
2023-05-07 21:33:54 +0530

    settings modified

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,6 @@
 //YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
 
 export const mongoConfig = {
-  // serverUrl: 'mongodb://localhost:27017/',
-  serverUrl: "mongodb+srv://simunidamodarreddy:RLvmD2b1bKdNMA3j@cluster0.njsifbs.mongodb.net/?retryWrites=true&w=majority",
+  serverUrl: 'mongodb://localhost:27017/',
   database: 'Pranav_Patel_Final_Project'
 };
